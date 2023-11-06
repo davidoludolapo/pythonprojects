1d728fa349e4f8c880113e337d1ca4e7c938da5d
unknown <davidoluwadolapo@gmail.com>
2023-11-06 02:07:41 +0100

    word dictionary app

---
+++ word_dictionary.py
@@ -0,0 +1,27 @@
+# Have a python dictionary that has a key/value pair that respresent a aword and it's definition
+# Collect ninput from the user, input is a word
+# check if the word is in the dictionary
+# Print the defintion
+
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+print(dictionary.meaning("eyes"))
+
+# def main():
+#     word_dictionary = {
+#         'hi': 'a way of greeting',
+#         'eyes': 'an organ for seeing',
+#         'earth': 'a planet in space'
+#     }
+
+#     while True:
+#         word  = input("Enter a word: ")
+#         if word =="":
+#             break
+#         if word in word_dictionary:
+#             print(word, ":", word_dictionary[word]) # [] is used to access the value associated with the 'word'
+
+# main()
+
