039ec0aefd8348fea3e3a625088a290412b2786e
unknown <davidoluwadolapo@gmail.com>
2023-11-09 17:57:29 +0100

    orking with functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+# def greet_user(username):
+#     """ Display a simple greeting ."""
+#     print(f"Hello {username}, how you doing")
+
+# greet_user("Tolu")
+
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+
+favorite_book("GOT")
+
+# Postional Arguments
+
+def describe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"\nMy {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'Hermione')
